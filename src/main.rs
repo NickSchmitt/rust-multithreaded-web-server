4edaf75353ce6cd2059174cc170098022a6a3b36
nickschmitt <nickschmitt@gmail.com>
2020-10-21 12:38:21 -0700

    added 404 error for invalid requests

--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,22 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-
         handle_connection(stream);
-        
     }
     
 }
 
 fn handle_connection(mut stream: TcpStream){
     let mut buffer = [0;1024];
-
     stream.read(&mut buffer).unwrap();
-
     let get = b"GET / HTTP/1.1\r\n";
-
-    if buffer.starts_with(get){
-
-        let contents = fs::read_to_string("hello.html").unwrap();
-    
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-    
+    let (status_line, filename) = if buffer.starts_with(get){
+        ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")}
+        else {
+            ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+        };
+        let contents = fs::read_to_string(filename).unwrap();
+        let response = format!("{}{}", status_line, contents);
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
-    } else {
-        // some other request
     }
-
-}
