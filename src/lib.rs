cadf6da77e59dd3f7b4fa343f3fb63291dc73965
nickschmitt <nickschmitt@gmail.com>
2020-10-22 16:42:37 -0700

    receiving and executing jobs in the worker's thread

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,7 @@ pub struct ThreadPool{
     sender: mpsc::Sender<Job>,
 }
 
-struct Job;
+type Job = Box<FnOnce() + Send + 'static>;
 impl ThreadPool {
     // Create a new threadpool
     // 
@@ -35,7 +35,14 @@ impl ThreadPool {
             sender,
         }
     }
-    pub fn execute<F>(&self, f: F) where F: FnOnce() + Send + 'static {} 
+
+    pub fn execute<F>(&self, f: F) 
+        where 
+            F: FnOnce() + Send + 'static 
+    { 
+        let job = Box::new(f); 
+        self.sender.send(job).unwrap();
+    } 
 }
 
 struct Worker {
@@ -45,8 +52,13 @@ struct Worker {
 
 impl Worker {
     fn new(id:usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(||{
-            receiver;
+        let thread = thread::spawn(move||{
+            loop {
+                let job = receiver.lock().unwrap().recv().unwrap();
+
+                println!("Worker {} got a job; executing.", id);
+                (*job)();
+            }
         });
 
         Worker {
