09aa311b624ce383889aac279752ec8bb922fb5b
nickschmitt <nickschmitt@gmail.com>
2020-10-22 16:31:05 -0700

    improving throughput with Threadpool and Worker structs; sharing receiving end of channel among workers using Arc and Mutex

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,16 @@ use std::net::TcpListener;
 use std::fs;
 use std::thread;
 use std::time::Duration;
+use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        pool.execute(||{
+            handle_connection(stream);
+        });
     }
     
 }
